4b8f6c165d2dcfd3915f67f267fdc5f12f8c74b4
jishenka <jiyeonshenka@gmail.com>
2023-02-06 15:29:16 +0900

    step-8: useAppLifecycleState

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -43,115 +41,36 @@ class App extends StatelessWidget {
 const url =
     "https://thumbs.dreamstime.com/b/ozero-malaya-ritsa-abkhazia-early-evening-lake-small-108638026.jpg";
 
-extension Normalize on num {
-  num normalized(
-    num selfRangeMin,
-    num selfRangeMax, [
-    num normalizedRangeMin = 0.0,
-    num normalizedRangeMax = 1.0,
-  ]) =>
-      (normalizedRangeMax - normalizedRangeMin) *
-          ((this - selfRangeMin) / (selfRangeMax - selfRangeMin)) +
-      normalizedRangeMin;
-}
-
-enum Action {
-  rotateLeft,
-  rotateRight,
-  moreVisible,
-  lessVisible,
-}
-
-@immutable
-class State {
-  final double rotationDeg;
-  final double alpha;
-
-  const State({
-    required this.rotationDeg,
-    required this.alpha,
-  });
-
-  const State.zero()
-      : rotationDeg = 0.0,
-        alpha = 1.0;
-
-  // reducers
-  State rotateRight() => State(
-        alpha: alpha,
-        rotationDeg: rotationDeg + 10.0,
-      );
-
-  State rotateLeft() => State(
-        alpha: alpha,
-        rotationDeg: rotationDeg - 10.0,
-      );
-
-  State moreVisible() => State(
-        alpha: min(alpha + 0.1, 1.0),
-        rotationDeg: rotationDeg,
-      );
-
-  State lessVisible() => State(
-        alpha: max(alpha - 0.1, 0.0),
-        rotationDeg: rotationDeg,
-      );
-}
-
-State reducer(State oldState, Action? action) {
-  switch (action) {
-    case Action.rotateLeft:
-      return oldState.rotateLeft();
-    case Action.rotateRight:
-      return oldState.rotateRight();
-    case Action.moreVisible:
-      return oldState.moreVisible();
-    case Action.lessVisible:
-      return oldState.lessVisible();
-    case null:
-      return oldState;
-  }
-}
-
 class HomeScreen extends HookWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final store = useReducer<State, Action?>(
-      reducer,
-      initialState: const State.zero(),
-      initialAction: null,
-    );
+    final state = useAppLifecycleState();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomePage'),
       ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: Action.values
-                .map((e) => TextButton(
-                      onPressed: () {
-                        store.dispatch(e);
-                      },
-                      child: Text(e.name),
-                    ))
-                .toList(),
-          ),
-          const SizedBox(height: 50),
-          Opacity(
-            opacity: store.state.alpha,
-            child: RotationTransition(
-              turns: AlwaysStoppedAnimation(
-                store.state.rotationDeg / 360.0,
+      body: Opacity(
+        opacity: state == AppLifecycleState.resumed ? 1.0 : 0.0,
+        child: Container(
+          margin: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 10,
+                color: Colors.black.withAlpha(100),
+                spreadRadius: 10,
               ),
-              child: Image.network(url),
-            ),
+            ],
           ),
-        ],
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset('assets/card.png'),
+          ),
+        ),
       ),
     );
   }
